4f5efb23420887530bd1e79dcf4aec68edc9cb6a
Tarcisio <tarcisiobm.dev@gmail.com>
2024-09-09 11:37:02 -0300

    Update remove image change.js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 function toggleMode() {
-    const html = document.documentElement
-    html.classList.toggle("light")
+    const html = document.documentElement;
+    html.classList.toggle("light");
     
-    const img = document.querySelector ("#profile img")
+    /*
+    const img = document.querySelector("#profile img");
     
-    if (html.classList.contains("light")){
-        img.setAttribute("src", "./assets/avatar-light.png")
+    if (html.classList.contains("light")) {
+        img.setAttribute("src", "./assets/avatar-light.png");
+    } else {
+        img.setAttribute("src", "./assets/avatar.png");
     }
-    
-        
-    else {
-        img.setAttribute("src", "./assets/avatar.png")
-    }
-    
-    
+    */
 }
